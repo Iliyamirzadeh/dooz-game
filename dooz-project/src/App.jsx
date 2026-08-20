73e95cb6bd9df811b605370bec4d88f67b3c6210
ilia <iliyamirzade@gmail.com>
2026-08-20 17:25:19 +0330

    changing the way of saving players moves that rematch button can work and restart the game and make app.jsx file cleaner

--- dooz-project/src/App.jsx
+++ dooz-project/src/App.jsx
@@ -11,7 +11,31 @@ const initialGameBoard = [
   [null, null, null],
   [null, null, null]
 ]
+function deriveGameBoard(gameTurns) {
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
+  for (const turn of gameTurns) {
+    const { square, player } = turn
+    const { row, col } = square
 
+    gameBoard[row][col] = player
+  }
+  return gameBoard;
+}
+function deriveWinner(gameBoard) {
+  let winner = null;
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column]
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column]
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column]
+
+    if (firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol) {
+      winner = firstSquareSymbol;
+    }
+  }
+  return winner;
+}
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X';
   if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
@@ -26,25 +50,10 @@ function App() {
   // const [activePlayer, setActivePlayer] = useState('X');
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
-  for (const turn of gameTurns) {
-    const { square, player } = turn
-    const { row, col } = square
+  const gameBoard = deriveGameBoard(gameTurns);
 
-    gameBoard[row][col] = player
-  }
-  let winner=null;
-  for (const combination of WINNING_COMBINATIONS) {
-    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column]
-    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column]
-    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column]
+  const winner = deriveWinner(gameBoard);
 
-    if (firstSquareSymbol &&
-      firstSquareSymbol === secondSquareSymbol &&
-      firstSquareSymbol === thirdSquareSymbol) {
-        winner=firstSquareSymbol;
-    }
-  }
   const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -63,14 +72,24 @@ function App() {
     });
   }
 
+  function handleRematch() {
+    setGameTurns([])
+  }
+
+
+
   return (
     <main>
       <div id='game-container'>
         <ol id='players' className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === 'X'}
+          />
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        {(winner || hasDraw ) && <GameOver winner={winner}/>}
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRematch} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
